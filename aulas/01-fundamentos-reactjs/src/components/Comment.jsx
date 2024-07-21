d5ba16d09cb25f042349d4268be3e1bf03ef647d
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2024-07-21 01:08:49 -0300

    Criação do componente de comentário

---
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment()
+{
+    return(
+        <div className = { styles.comment } >
+            <img src="https://avatars.githubusercontent.com/u/52306002?v=4" alt="Foto de avatar" />
+
+            <div className = { styles.commentBox } >
+                <div className = {styles.commentContent} >
+                <header>
+                    <div className = { styles.authorAndTime }>
+                        <strong>Germain Girndt</strong>
+                        <time title = "20 de julho às 20:37h" dateTime = "2023-07-20 20:36:56">Cerca de 1h atrás</time>
+                    </div>
+                    <button title = "Deletar comentário" >
+                        <Trash size = {20} />
+                    </button>
+                </header>
+                <p>Muito bom Dev, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>   
+        </div>
+    )
+}
