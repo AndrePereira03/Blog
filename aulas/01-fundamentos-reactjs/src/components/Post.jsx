b0a9e7aa25d45732748c90f3e9c372ddf761ad3a
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2025-10-16 23:06:10 -0300

    Removendo comentários

--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -38,6 +38,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText("");
   }
 
+  function deleteComment(comment) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment != commentsWithoutDeletedOne;
+    });
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -60,10 +68,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type == "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type == "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="">{line.content}</a>
               </p>
             );
@@ -86,7 +94,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
