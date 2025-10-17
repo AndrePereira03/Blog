2945d5a17ec34c1d72118f69a2444b5c5215f7a6
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2025-10-16 22:24:07 -0300

    Adicionando comentários

--- aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar
@@ -27,7 +27,7 @@ export function Comment() {
               <Trash size={2} />
             </button>
           </header>
-          <p>Muito bom Dev, parabéns!!</p>
+          <p>{content}</p>
         </div>
 
         <footer>
